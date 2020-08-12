74eda076777474a3e1e4745f1b9ea3f7495baa05
Ange Batie <ange.batie@gmail.com>
2020-08-12 04:46:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,8 +14,10 @@ def monopoly_with_second_tier
   # complexity.
   {
     :railroads => {
-      pi
-    }
+      :pieces => 4,
+      :rent_in_dollars => {},
+      :names => {}
+      }
   }
 end
 
